437dc2b391e6f0a194065f6d4f77d79b1159c34b
khanaamir1102 <khanaamir1102@gmail.com>
2021-03-22 12:46:34 +0530

    collection program

---
+++ Intellije/src/com/basic/HashMapCollectioin.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HashMapCollectioin {
+	public static void main(String[] args) {
+		Map<String, String>map=new TreeMap<>();
+		map.put("a", "america");
+		map.put("b", null);
+		map.put("c", "a");
+		map.put("d", "dehradun");
+		map.put("e", null);
+		map.put("f", "foriegn");
+		for(Map.Entry m:map.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+		
+	}
+
+}
