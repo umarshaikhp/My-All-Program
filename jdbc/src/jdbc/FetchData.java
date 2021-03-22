eab48d30e29cd7e822230f414d92b4a33607f42c
khanaamir1102 <khanaamir1102@gmail.com>
2021-03-22 14:25:44 +0530

    jdbc

---
+++ jdbc/src/jdbc/FetchData.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.*;
+
+public class FetchData {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Room", "root", "root");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from dost");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+
+		}
+
+	}
+}
